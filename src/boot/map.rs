f0d4154e4fd97a71d7fc9bd4f17c55cabf6c3bc9
Jeremy Karagania <jeremykaragania@icloud.com>
2026-09-08 15:59:24 +0800

    Add memory attributes to descriptor creation
    - Now we can specify attributes for block and page descriptors. We
      haven't really abstracted over what's in the VMSA, instead we omit
      attributes that we don't care about.

--- src/boot/map.rs
+++ src/boot/map.rs
@@ -1,4 +1,15 @@
-use crate::mm::table::{Descriptor, level_entry_size, level_shift};
+use crate::mm::table::{
+    AccessPermissions, Descriptor, Execution, MemoryAttributes, level_entry_size, level_shift,
+};
+
+const TEXT_MEM_ATTRIBUTES: MemoryAttributes = MemoryAttributes {
+    permissions: AccessPermissions::ReadOnly,
+    execution: Execution::Executable,
+};
+const DATA_MEM_ATTRIBUTES: MemoryAttributes = MemoryAttributes {
+    permissions: AccessPermissions::ReadWrite,
+    execution: Execution::NonExecutable,
+};
 
 unsafe extern "C" {
     static text_begin: u8;
@@ -25,7 +36,12 @@ unsafe fn get_or_create_table(
     entry.addr(level) as *mut Descriptor
 }
 
-pub unsafe fn map_block(mut table: *mut Descriptor, begin: u64, end: u64) -> usize {
+pub unsafe fn map_block(
+    mut table: *mut Descriptor,
+    begin: u64,
+    end: u64,
+    attrs: &MemoryAttributes,
+) -> usize {
     let mut entry_begin = 0;
 
     for level in 0..=3 {
@@ -39,7 +55,7 @@ pub unsafe fn map_block(mut table: *mut Descriptor, begin: u64, end: u64) -> usi
         let entry_end = entry_begin + entry_size as u64;
 
         if entry_end <= end && level > 0 {
-            unsafe { *entry = Descriptor::block(begin, level).into() };
+            unsafe { *entry = Descriptor::block(begin, level, attrs).into() };
 
             return (entry_end - begin) as usize;
         }
@@ -50,9 +66,14 @@ pub unsafe fn map_block(mut table: *mut Descriptor, begin: u64, end: u64) -> usi
     unreachable!();
 }
 
-pub unsafe fn map_region(table: *mut Descriptor, mut begin: u64, end: u64) {
+pub unsafe fn map_region(
+    table: *mut Descriptor,
+    mut begin: u64,
+    end: u64,
+    attrs: &MemoryAttributes,
+) {
     while begin < end {
-        let consumed = unsafe { map_block(table, begin, end) };
+        let consumed = unsafe { map_block(table, begin, end, attrs) };
         begin += consumed as u64;
     }
 }
@@ -62,10 +83,10 @@ pub extern "C" fn create_init_mapping(table: *mut u64) {
     unsafe {
         let begin = &text_begin as *const u8 as u64;
         let end = &data_begin as *const u8 as u64;
-        map_region(table as *mut Descriptor, begin, end);
+        map_region(table as *mut Descriptor, begin, end, &TEXT_MEM_ATTRIBUTES);
 
         let begin = &data_begin as *const u8 as u64;
         let end = &data_end as *const u8 as u64;
-        map_region(table as *mut Descriptor, begin, end);
+        map_region(table as *mut Descriptor, begin, end, &DATA_MEM_ATTRIBUTES);
     }
 }
--- src/mm/table.rs
+++ src/mm/table.rs
@@ -9,6 +9,23 @@ pub enum DescriptorKind {
     Block,
 }
 
+#[derive(PartialEq, Eq)]
+pub enum AccessPermissions {
+    ReadOnly,
+    ReadWrite,
+}
+
+#[derive(PartialEq, Eq)]
+pub enum Execution {
+    Executable,
+    NonExecutable,
+}
+
+pub struct MemoryAttributes {
+    pub permissions: AccessPermissions,
+    pub execution: Execution,
+}
+
 #[derive(Clone, Copy)]
 pub struct Descriptor(u64);
 
@@ -42,9 +59,20 @@ impl Descriptor {
         Self(0)
     }
 
-    pub fn block(addr: u64, level: u8) -> Self {
+    pub fn block(addr: u64, level: u8, attrs: &MemoryAttributes) -> Self {
         let kind_bits = { if level == 3 { 0b11 } else { 0b01 } };
-        Self(addr & (u64::MAX << level_shift(level)) | 1 << 10 | kind_bits)
+        // AP[1] honestly seems kind of useless. We have to translation tables for a reason right?
+        let permission_bits = ((attrs.permissions == AccessPermissions::ReadOnly) as u64) << 2 | 1;
+        let xn_bit = (attrs.execution == Execution::NonExecutable) as u64;
+        let execution_bits = xn_bit << 54 | xn_bit << 53;
+
+        Self(
+            addr & (u64::MAX << level_shift(level))
+                | execution_bits
+                | permission_bits
+                | 1 << 10
+                | kind_bits,
+        )
     }
 
     pub fn table(addr: u64) -> Self {
